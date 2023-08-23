b17ae18ccdaf5e152190a3ad5950f0e3e5ad3af3
zzzzseong <jung48182@gmail.com>
2023-08-23 22:06:09 +0900

    feat(Exception): 서비스에 필요한 exception 추가

--- src/main/java/com/Qlog/backend/exception/CustomErrorCode.java
+++ src/main/java/com/Qlog/backend/exception/CustomErrorCode.java
@@ -13,7 +13,16 @@ public enum CustomErrorCode {
     SIGNATURE_EXCEPTION("[지원하지 않는 토큰] 잘못된 요청입니다."),
     ILLEGAL_ARGUMENT_EXCEPTION("[지원하지 않는 토큰] 잘못된 요청입니다."),
 
-    PAGE_NOT_FOUND_EXCEPTION("존재하지 않는 페이지입니다.");
+    PAGE_NOT_FOUND_EXCEPTION("존재하지 않는 페이지입니다."),
+
+    NON_START_BEARER_EXCEPTION("[잘못된 형식의 토큰] 클라이언트 토큰 요청 형식이 잘못되었습니다."),
+
+    //Jwt Token Exception
+    JWT_NOT_FOUND_EXCEPTION("[토큰을 찾을 수 없음] 접근 권한이 없습니다. 로그인 해주세요."), //토큰 없이 접근할때 발생
+
+    //Auth Exception
+    AUTHENTICATION_EXCEPTION("로그인 정보가 일치하지 않습니다."),
+    ALREADY_LOGOUT_EXCEPTION("이미 로그아웃 되었습니다.");
 
     private final String statusMessage;
 }
